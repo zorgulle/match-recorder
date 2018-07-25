b8dba11eea453ce86fee5a7814f7efeb24454d9e
Adrien <winy95battle@gmail.com>
2018-07-26 01:52:08 +0200

    Remove

--- player/__init__.py
+++
@@ -1,3 +0,0 @@
-from flask_api import FlaskAPI as Flask
-
-app = Flask(__name__)
--- player/utils/decorator.py
+++
@@ -1,8 +0,0 @@
-def singleton(cls):
-    instance = None
-    def ctor(*args, **kwargs):
-        nonlocal instance
-        if not instance:
-            instance = cls(*args, **kwargs)
-        return instance
-    return ctor
